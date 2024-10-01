7c8f634c3736f25f1e33218b382e1187f3f8689f
Abdelhakim <faddaoui.abdelhakim@gmail.com>
2024-10-01 14:49:20 +0100

    creating of three functions

--- vbai inspection.c
+++ vbai inspection.c
@@ -1,9 +1,9 @@
 //==============================================================================
 //
 // Title:		vbai inspection
-// Purpose:		A short description of the application.
+// Purpose:		.
 //
-// Created on:	9/27/2024 at 4:34:57 PM by .
+// Created on:	9/27/2024 at 4:34:57 PM by abdelhakim faddaoui
 // Copyright:	. All Rights Reserved.
 //
 //==============================================================================
@@ -13,75 +13,151 @@
 
 #include "VBAIInterfaceC.h"
 #include <ansi_c.h>
-#include <cvirte.h>		
+#include <cvirte.h>
 #include <userint.h>
 #include "vbai inspection.h"
 #include "toolbox.h"
 #include "vbai inspection.h"
 
-
 //==============================================================================
 // Constants
+//==============================================================================
 
 //==============================================================================
 // Types
+//==============================================================================
 
 //==============================================================================
 // Static global variables
+//==============================================================================
 
 static int panelHandle = 0;
 
 //==============================================================================
 // Static functions
+//==============================================================================
 
 //==============================================================================
 // Global variables
+//==============================================================================
 
 //==============================================================================
 // Global functions
+//==============================================================================
+int LaunchVbaiAndOpenConnexion(char Engine_Name[512], VBAISession *vbSession);
+int selectInspection(VBAISession vbSession, char inspectionPath[]);
+int getMeasurement(VBAISession vbSession, bool32 *inspectionStatus);
+
+
+/// HIFN Document your function here.
+/// HIFN You may use multiple lines for documentation.
+/// HIRET Document return value here.
+/// HIRET You may use multiple lines for documentation.
+/// HIPAR Engine_Name/Document parameter here.
+/// HIPAR vbSession/Document parameter here.
+int LaunchVbaiAndOpenConnexion (char Engine_Name[512], VBAISession *vbSession)
+{
+	VBAIError vbError;
+	String255 Session_Name;
+	int oldValue = SetBreakOnLibraryErrors (0);
+	
+	//vbError = vbaiLaunchLocalVBAIEngine (Engine_Name, NULL, 1, Session_Name, sizeof (String255));
+	vbError = vbaiLaunchLocalVBAIEngine (Engine_Name, NULL, 1, Session_Name, sizeof (String255));
+	if (vbError != 	VBAIErrorSuccess)
+		goto ERROR;
+
+	vbError = vbaiOpenConnection (Session_Name, NULL, 1, vbSession);
+	if (vbError != 	VBAIErrorSuccess)
+		goto ERROR;
+
+	return 0;
+ERROR:
+	SetBreakOnLibraryErrors (oldValue);
+	return -1;
+
+}
+
+/// HIFN Document your function here.
+/// HIFN You may use multiple lines for documentation.
+/// HIRET Document return value here.
+/// HIRET You may use multiple lines for documentation.
+/// HIPAR vbSession/Document parameter here.
+/// HIPAR inspectionPath/Document parameter here.
+int selectInspection (VBAISession vbSession, char inspectionPath[])
+{
+	VBAIError vbError;
+	int oldValue = SetBreakOnLibraryErrors (0);
+
+	vbError = vbaiOpenInspection (vbSession, inspectionPath);
+	if (vbError != 	VBAIErrorSuccess)
+	{
+		SetBreakOnLibraryErrors (oldValue);
+		return -1;
+	}
+
+	SetBreakOnLibraryErrors (oldValue);
+	return 0;
+}
+
+
+/// HIFN Document your function here.
+/// HIFN You may use multiple lines for documentation.
+/// HIRET Document return value here.
+/// HIRET You may use multiple lines for documentation.
+/// HIPAR vbSession/Document parameter here.
+/// HIPAR inspectionStatus/Document parameter here.
+int getMeasurement (VBAISession vbSession, bool32 * inspectionStatus)
+{
+	VBAIError vbError;
+	VBAIDateTime lastUpdatedTime;
+	VBAIStepResult stepResults[20];
+	VBAIMeasurements stepMeasurements[6][10];
+	int oldValue = SetBreakOnLibraryErrors (0);
+
+	vbError = vbaiEnableInspectionMeasurements(vbSession, 1);
+	if (vbError != 	VBAIErrorSuccess)
+		goto Error;
+
+	vbError = vbaiRunInspectionOnce(vbSession, 10000);
+	if (vbError != 	VBAIErrorSuccess)
+		goto Error;
+
+	vbError = vbaiGetInspectionMeasurements(vbSession, NULL, 0, stepMeasurements, &lastUpdatedTime);
+	if (vbError != 	VBAIErrorSuccess)
+		goto Error;
+
+	vbError = vbaiGetInspectionResults(vbSession, stepResults, NULL , &lastUpdatedTime, inspectionStatus);
+	if (vbError != 	VBAIErrorSuccess)
+		goto Error;
+
+	vbaiDispose(stepMeasurements);
+
+	SetBreakOnLibraryErrors (oldValue);
+
+	return 0;
+
+Error:
+	vbaiDispose(stepMeasurements);
+	SetBreakOnLibraryErrors (oldValue);
+	return -1;
+}
 
 /// HIFN The main entry-point function.
 int main (int argc, char *argv[])
 {
-	int i = 0;
-	
 	int error = 0;
-	char Session_Name[512] = "Untitled1";
-	char Engine_Name[512] = "Etoggle";
-	//int Session_Name_Length = 512;
-	int Session_Name_Length = sizeof (String255);
+	int status = 0;
 	VBAISession vbSession;
-	VBAIError vbError;
-	int oldValue;
-	VBAIMeasurements stepMeasurements[6][10];
-	VBAIDateTime lastUpdatedTime;
+	bool32 inspectionStatus;
 	
-	oldValue = SetBreakOnLibraryErrors (0);
-
 	/* initialize and load resources */
 	nullChk (InitCVIRTE (0, argv, 0));
 	errChk (panelHandle = LoadPanel (0, "vbai inspection.uir", PANEL));
 
-	// lunch VBAI 
-	for (i=0,vbError =-1; (i<10 && vbError != VBAIErrorSuccess); ++i)
-	{
-		vbError = vbaiLaunchLocalVBAIEngine (Engine_Name, VBAI_2023, 1, Session_Name, sizeof (String255));
-	}
 	
-	printf ("%d\n", i);
-	
-	vbError = vbaiOpenConnection (Session_Name, NULL, 1, &vbSession);
-	vbError = vbaiOpenInspection (vbSession, "C:\\Users\\EOLT E-TOGGLE\\Desktop\\job camera ST80\\job bumper.vbai");
-	vbError = vbaiEnableInspectionMeasurements(vbSession, 1);
-	vbError = vbaiRunInspectionOnce(vbSession, 10000);
-	
-	vbError = vbaiGetInspectionMeasurements(vbSession, NULL, 0, stepMeasurements, &lastUpdatedTime);
-	vbError = vbaiGetInspectionResults(vbSession, VBAIStepResult stepResults[], size_t* size, VBAIDateTime* lastUpdatedTime, bool32* inspectionStatus);
-
-	
-	vbaiDispose(stepMeasurements);
-	
-	SetBreakOnLibraryErrors (oldValue);
+	status = LaunchVbaiAndOpenConnexion("Etoggle", &vbSession);
+	status = selectInspection(vbSession, ".\\inspectionCamera01.vbai");
+	status = getMeasurement(vbSession, &inspectionStatus);
 	
 	/* display the panel and run the user interface */
 	errChk (DisplayPanel (panelHandle));
@@ -99,7 +175,7 @@ Error:
 
 /// HIFN Exit when the user dismisses the panel.
 int CVICALLBACK panelCB (int panel, int event, void *callbackData,
-		int eventData1, int eventData2)
+						 int eventData1, int eventData2)
 {
 	if (event == EVENT_CLOSE)
 		QuitUserInterface (0);
